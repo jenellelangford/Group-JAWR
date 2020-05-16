b0d6244988767a0b2461240cfe2d462ef089657a
William Rave <wrave@gwu.edu>
2020-05-16 14:18:05 -0400

    Schema table

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,33 @@ CREATE DATABASE light_db;
 
 USE light_db;
 
-CREATE TABLE workers (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  username VARCHAR(40),
-  user_password VARCHAR(40),
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  avatar_src VARCHAR(40),
+  first_name VARCHAR(40),
+  last_name VARCHAR(40),
   email VARCHAR(40),
   user_desc TEXT,
-  photo_src TEXT 
+  user_password VARCHAR(30),
+  user_venmo VARCHAR(30),
+  user_location VARCHAR(30)
+)
+
+CREATE TABLE workers (
+  worker_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER NOT NULL,
+  requests TEXT,
+  skills TEXT
+);
+
+CREATE TABLE patrons (
+  patron_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER NOT NULL,
+);
+
+CREATE TABLE coders (
+  coder_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER NOT NULL,
+  speciality VARCHAR(40),
+  skills TEXT
 );
