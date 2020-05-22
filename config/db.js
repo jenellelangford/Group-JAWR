707293a6a2ec46f6168e655bd204738ce4b5e602
Rachel Murray <dcbeergoddess@gmail.com>
2020-05-22 10:21:31 -0400

    re-pushing jenelle css changes and heroku connection info

--- config/db.js
+++ config/db.js
@@ -4,14 +4,14 @@ module.exports = {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "12345",
+    password: "password",
     database: "light_db"
   },
   production: {
-    host: "localhost",
+    host: "pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "12345",
-    database: ""
+    user: "oew35naq5az79z9x",
+    password: "om9iv7rdijlt5ks7",
+    database: "hkxqp4ptezil8qei"
   }
   }
